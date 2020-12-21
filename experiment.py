gradient waveform is not correct

experiment.py
@@ -102,6 +102,7 @@ class Experiment:
             gradb_class = gb.GPAFHDO
         self.gradb = gradb_class(grad_t, grad_channels, self.server_command, spi_freq)
         self.grad_channels = grad_channels
+        self.shims = np.zeros(grad_channels)
         
         self.acq_retry_limit = acq_retry_limit
 
@@ -207,6 +208,9 @@ class Experiment:
         self.tx_bytes[3::4] = (tx_q >> 8).astype(np.uint8).tobytes()
         self.tx_data_dirty = False
 
+    def set_shim_currents(self, shims):
+        self.shims = shims
+
     def compile_grad_data(self):
         """ go through the grad data and prepare binary array to send to the server """
         if not hasattr(self, 'grad_data'):
@@ -215,7 +219,7 @@ class Experiment:
         if self.grad_data[0].size == 0:
             self.grad_data = [np.array([0]) for k in range(self.grad_channels)]
 
-        grad_bram_data = self.gradb.float2bin(self.grad_data) # grad board-specific transformation
+        grad_bram_data = self.gradb.float2bin(self.grad_data, self.shims) # grad board-specific transformation
             
         self.grad_bytes = grad_bram_data.tobytes()
         self.grad_data_dirty = False

grad_board.py
@@ -95,7 +95,8 @@ class OCRA1:
         # Fill more in here
         pass
 
-    def float2bin(self, grad_data):
+    def float2bin(self, grad_data, shims):
+        # TODO: implement shimming
         grad_bram_data = np.zeros(grad_data[0].size * self.grad_channels, dtype=np.uint32)
 
         for ch, gd in enumerate(grad_data):
@@ -197,7 +198,7 @@ class GPAFHDO:
 
     def ampere_to_dac_code(self, ampere):
         v_ref = 2.5
-        dac_code = int( (ampere / self.gpa_current_per_volt + v_ref)/5 * 0xffff)
+        dac_code = ( (np.float32(ampere) / self.gpa_current_per_volt + v_ref)/5 * 0xffff).astype(np.int)
         return dac_code    
 
     def calibrate(self,
@@ -242,12 +243,14 @@ class GPAFHDO:
                 plt.grid(True)
                 plt.show()    
 
-    def float2bin(self, grad_data):
+    def float2bin(self, grad_data, shims):
+        # grad_data is normalized to max current of the GPA
         grad_bram_data = np.zeros(grad_data[0].size * self.grad_channels, dtype=np.uint32)
 
         for ch, gd in enumerate(grad_data):
             # Not 2's complement - 0x0 word is ~0V (-10A), 0xffff is ~+5V (+10A)
             gr_dacbits = np.round(32767.49 * (gd + 1)).astype(np.uint32) & 0xffff
+            gr_dacbits_shims = self.ampere_to_dac_code(np.ones(gd.shape[0]))
             gr_dacbits_cal = self.calculate_corrected_dac_code(ch,gr_dacbits)                            
             gr = gr_dacbits_cal | 0x80000 | (ch << 16)
             

test_gpa_fhdo.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     B_per_m_per_current = 0.02 # T/m/A, approximate value for tabletop gradient coil
 
     # values for gpa fhdo
-    gpa_current_per_volt = 3.75 # A/V, theoretical value for gpa fhdo without 2.5 V offset!
+    gpa_current_per_volt = 2.5 # A/V, theoretical value for gpa fhdo without 2.5 V offset!
     max_dac_voltage = 2.5
 
     # values for ocra1
@@ -33,15 +33,19 @@ if __name__ == "__main__":
 
     max_Hz_per_m = max_dac_voltage * gpa_current_per_volt * B_per_m_per_current * gamma	
     # grad_max = max_Hz_per_m # factor used to normalize gradient amplitude, should be max value of the gpa used!	
-    grad_max = 16 # unrealistic value used only for loopback test
+    grad_max = 16 # random value used only for testing
 
     rf_amp_max = 10 # factor used to normalize RF amplitude, should be max value of system used!
     ps = PSAssembler(rf_center=lo_freq*1e6,
         # how many Hz the max amplitude of the RF will produce; i.e. smaller causes bigger RF V to compensate
         rf_amp_max=rf_amp_max,
         grad_max=grad_max,
+        addresses_per_grad_sample=3,
         clk_t=clk_t,
         tx_t=tx_t,
+        grad_pad = 1,
+        pulseq_t_match=True,
+        adc_pad=70,
         grad_t=grad_interval)
     tx_arr, grad_arr, cb, params = ps.assemble('test_gpa_fhdo.seq', byte_format=False)
 
@@ -63,6 +67,10 @@ if __name__ == "__main__":
     exp.add_tx(tx_arr)
     exp.add_grad(grad_arr)
 
+    exp.gradb.init_hw()
+    exp.set_shim_currents(np.array([0,0,0,0]))
+    plt.plot(grad_arr[0]);plt.show()
+
     exp.gradb.calibrate(max_current = 2,
         num_calibration_points=10,
         gpa_current_per_volt=gpa_current_per_volt,
@@ -79,6 +87,7 @@ if __name__ == "__main__":
     wait = input('All gradient channels are set to {:f} A. Press Enter to continue.'.format(current))
 
     data = exp.run() # Comment out this line to avoid running on the hardware
+    wait = input('All gradient channels are set to shim current. Press Enter to continue.'.format(current))
 
     # set all channels back to 0 A
     for ch in range(num_grad_channels):